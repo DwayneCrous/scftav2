9911f048055141709d4992eba5867093ad7a19e9
DwayneCrous <dwaynecrous206@gmail.com>
2025-11-30 20:11:06 +0200

    re-init: Switched from Svelte to react and nextjs

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AMOs51M9O38oVUzDMadHgxVokxRekfLlfB28Ee43dzs=\"\n}"
---
+++ .next/dev/static/chunks/scftav2_app_page_tsx_b4c79364._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/f1ea3_next_dist_626c9cf0._.js"
+],
+    source: "dynamic"
+});
