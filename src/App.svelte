2629ab1e48dbc9c606ab9034e0d17a1fc75885da
DwayneCrous <dwaynecrous206@gmail.com>
2025-11-30 09:28:01 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Header from "./components/Header.svelte";
+  import HeroSection from "./components/HeroSection.svelte";
+</script>
+
+<main>
+  <Header />
+  <HeroSection />
+</main>
+
+<style>
+</style>
---
+++ src/components/HeroSection.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="hero-header">
+    <h1 class="hero-text">Track finances easily <br />with maximum security</h1>
+  </div>
+</main>
+
+<style>
+  .hero-header {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    margin-top: 0px;
+    background-color: #333;
+  }
+
+  .hero-text {
+    font-size: 4rem;
+    color: white;
+    margin: 20px;
+  }
+</style>
